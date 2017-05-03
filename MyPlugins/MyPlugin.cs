8e6feece895cd9fb3d1e351f69774be13b9b4ce9
denizhankirmaci <denizhankirmaci@gmail.com>
2017-05-03 14:28:38 +0300

    asd
    asd

---
+++ MyPlugins/MyPlugin.cs
@@ -0,0 +1,25 @@
+﻿using Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPlugins
+{
+    public class MyPlugin : Plugin
+    {
+        public string Name()
+        {
+            return "MyPlugin";
+        }
+
+        public void Do()
+        {
+            //MessageBox.Show("Denizhan Deneme");
+            ToolStripMenuItem ts = new ToolStripMenuItem();
+            ts.Name = "Human Resources";
+        }
+    }
+}
---
+++ Plugin_Evidea/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plugin_Evidea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load Plugin Assembly
+            string appPath = Application.StartupPath + "\\Plugin\\MyPlugins.dll";
+            Assembly asm = System.Reflection.Assembly.LoadFile(appPath);
+            foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type item in a.GetTypes())
+                {
+                    if (item.GetInterface("MyPlugin") != null)
+                    {
+                        MessageBox.Show("DKK");
+                    }
+                }
+            }
+        }
+        
+        private void menuLoad_Click(object sender, EventArgs e)
+        {
+            //bool control = true;
+            foreach (ToolStripItem item in menuStrip1.Items)
+            {
+                if (item.Text == "Human Resources")
+                //{
+                    //control = false;
+                    return;
+                //}
+            }
+            //if (control == true)
+            //{
+                ToolStripMenuItem tsmi = new ToolStripMenuItem();
+                tsmi.Text = "Human Resources";
+                tsmi.DropDown.Items.Add("Emploies");
+                tsmi.DropDown.Items.Add("Recruitment");
+                menuStrip1.Items.Insert(0, tsmi);
+                //menuStrip1.Items.Add(tsmi);
+            //}
+        }
+
+        private void menuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
